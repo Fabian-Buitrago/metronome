a8a520ec2d5859e8044c44aca2e617a8613efc63
fbuitrago <fabianbuitragov@gmail.com>
2023-05-02 18:55:05 -0500

    Create song model

---
+++ src/models/song.js
@@ -0,0 +1,8 @@
+export default class Song {
+  constructor(title, artist, duration, bpm) {
+    this.title = title;
+    this.artist = artist;
+    this.duration = duration;
+    this.bpm = bpm;
+  }
+}
